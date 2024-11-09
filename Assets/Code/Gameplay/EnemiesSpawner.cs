f6007e5ecc2f595580cbc6f587c3c04abd464302
ArtemPylhun <artem.pylhun@oa.edu.ua>
2024-11-09 12:07:49 +0200

    feat: enemies spawner implemented

---
+++ Assets/Code/Gameplay/EnemiesSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Gameplay
+{
+    public class EnemiesSpawner:MonoBehaviour
+    {
+        [SerializeField]
+        private float _spawnInterval = 2;
+        
+        [SerializeField]
+        private GameObject _enemy;
+        
+        private float _timer;
+
+        private void Start() => 
+            _timer = _spawnInterval;
+        
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <= 0)
+            {
+                SpawnEnemy();
+                _timer = _spawnInterval;
+            }
+        }
+
+        private void SpawnEnemy()
+        {
+            Instantiate(_enemy, gameObject.transform);
+        }
+    }
+}
