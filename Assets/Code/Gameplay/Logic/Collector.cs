86408acbb8429f35ee2ee95e52fa55ad39c20ded
ArtemPylhun <artem.pylhun@oa.edu.ua>
2024-11-09 12:29:49 +0200

    feat: basic object collection implemented

---
+++ Assets/Code/Gameplay/Logic/Collector.cs
@@ -0,0 +1,16 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Code.Gameplay.Logic
+{
+    internal sealed class Collector: MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.TryGetComponent(out ICollectable collectable))
+            {
+                collectable.Collect();
+            }
+        }
+    }
+}
---
+++ Assets/Code/Gameplay/Logic/Mace.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Code.Gameplay.Logic
+{
+    public class Mace : MonoBehaviour, ICollectable
+    {
+        public void Collect()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
